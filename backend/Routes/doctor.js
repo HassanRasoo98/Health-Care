fd90b96bcf0244bf04dee2c48f364015f1430f41
mwassayahmad123 <113931206+mwassayahmad123@users.noreply.github.com>
2024-08-10 02:41:55 +0500

    API's Schema Completed

--- backend/Routes/doctor.js
+++ backend/Routes/doctor.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import {updateDoctor, deleteDoctor, getAllDoctor, getSingleDoctor} from '../Controllers/doctorController.js'
 import { authenticate, restrict } from '../auth/verifyToken.js'
-
+import reviewRouter from './review.js'
 
 const router = express.Router()
+
+//nested routes
+router.use('/:doctorId/reviews', reviewRouter)
 
 router.get('/:id', getSingleDoctor)
 router.get('/', getAllDoctor)
